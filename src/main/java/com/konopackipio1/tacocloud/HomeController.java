31ef51258d680c88176cba59230706c3d711a08e
pkonopacki1 <konopackipio@gmail.com>
2022-05-04 07:57:23 +0200

    Added: - HomeController.java and required templates and statics

---
+++ src/main/java/com/konopackipio1/tacocloud/HomeController.java
@@ -0,0 +1,14 @@
+package com.konopackipio1.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public static String home() {
+        return "home";
+    }
+    
+}
--- src/main/java/com/konopackipio1/tacocloud/TacoCloudApplication.java
+++ src/main/java/com/konopackipio1/tacocloud/TacoCloudApplication.java
@@ -3,6 +3,9 @@ package com.konopackipio1.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//-----Entry point class-----//
+
+// Very important annotation combining @SpringBootConfiguration, @EnableAutoConfiguration, @ComponentScan
 @SpringBootApplication
 public class TacoCloudApplication {
 
